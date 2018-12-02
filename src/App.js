4a6b5e41e4f36678696b57dc1e97f606c839f7e4
Dennis <deweis@gmx.ch>
2018-12-02 22:33:36 +0100

    Add Fontawesome icons

--- src/App.js
+++ src/App.js
@@ -17,11 +17,33 @@ class App extends Component {
                     <div className="row">
                       <div className="col s6">
                         <h5>Break Length</h5>
-                        <div style={{ margin: '10px' }}>Up 5 Down</div>
+                        <div style={{ margin: '10px' }}>
+                          <i class="fas fa-arrow-down" />
+                          <span
+                            style={{
+                              padding: '15px',
+                              fontSize: '1.3em'
+                            }}
+                          >
+                            5
+                          </span>
+                          <i class="fas fa-arrow-up" />
+                        </div>
                       </div>
                       <div className="col s6">
                         <h5>Session Length</h5>
-                        <div style={{ margin: '10px' }}>Up 25 Down</div>
+                        <div style={{ margin: '10px' }}>
+                          <i class="fas fa-arrow-down" />
+                          <span
+                            style={{
+                              padding: '15px',
+                              fontSize: '1.3em'
+                            }}
+                          >
+                            25
+                          </span>
+                          <i class="fas fa-arrow-up" />
+                        </div>
                       </div>
                     </div>
                   </div>
